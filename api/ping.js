087ccd1b0a25a8cd19ac6622126ec05d4217b5de
kazentek <kazentek.contact@gmail.com>
2026-03-22 22:22:53 +0100

    Create ping.js

---
+++ api/ping.js
@@ -0,0 +1,14 @@
+/**
+ * GET /api/ping
+ * Lightweight keepalive endpoint
+ * Called every 5 minutes by an external pinger to prevent cold starts
+ */
+module.exports = function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Cache-Control", "no-store");
+  res.status(200).json({
+    ok: true,
+    ts: Date.now(),
+    uptime: Math.round(process.uptime()) + "s",
+  });
+};
