877cc1505d6c30fc51d533c190b94fb9b2eca076
kazentek <kazentek.contact@gmail.com>
2026-03-23 12:09:16 +0100

    Add files via upload

---
+++ public/pixel.js
@@ -0,0 +1,18 @@
+/**
+ * Facebook Pixel — served as static file (no cold start)
+ * Replace YOUR_PIXEL_ID with your actual Pixel ID
+ * Redeploy after changing
+ */
+(function() {
+  window.__FB_PIXEL_ID__ = "YOUR_PIXEL_ID_HERE";
+
+  !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){
+    n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];
+    t=b.createElement(e);t.async=!0;t.src=v;
+    s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s);
+  }(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
+
+  fbq('init', 'YOUR_PIXEL_ID_HERE');
+  fbq('track', 'PageView');
+})();
